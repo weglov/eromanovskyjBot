4a3f736fa94b441fd70b545346a69cf2c3da9f38
Viktor Shcheglov <v-shcheglov@yandex-team.ru>
2019-05-31 23:36:44 +0300

    test

--- config.py
+++ config.py
@@ -2,5 +2,14 @@ from datetime import datetime
 
 
 class Config:
-    token = "688228040:AAGgSKCw9_XsoAKQODvzrY4R05mq8jTWY3U"
+    env_key = 'TELE_KEY'
+    users = ['eromanovskyj', 'dm_melnikov']
     old_date = datetime.today().timestamp() - 280
+    phrases = [
+        "пернул мозгом",
+        "да, ништяк",
+        "хуй в кармане ночевал",
+        "ключи от жопы потерял",
+        "ебануться туфли гнутся",
+        "кому тут отстрочить"
+    ]
