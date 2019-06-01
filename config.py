08c8850ddcfb72ee3f58878a49a50107b56a368f
Viktor Shcheglov <v-shcheglov@yandex-team.ru>
2019-06-01 21:42:51 +0300

    update config and added min length

--- config.py
+++ config.py
@@ -3,6 +3,9 @@ from datetime import datetime
 
 class Config:
     env_key = 'TELE_KEY'
+    lang_to = 'be'
+    lang_from = 'ru'
+    time_offset = 300 # секунд до повторной отправки (5мин)
     users = ['eromanovskyj', 'dm_melnikov']
     old_date = datetime.today().timestamp() - 280
     phrases = [
