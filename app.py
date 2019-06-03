4a3eba32a7b8914de741a21d72b8b9795428ceb4
akhachatryan <Albert.Khachatriyan@ingrammicro.com>
2019-06-03 17:13:45 +0300

    New answer flow and added period

--- app.py
+++ app.py
@@ -3,49 +3,78 @@ import random
 from translate import translator
 import os
 from config import Config
+from collections import namedtuple
+
 
 config = Config()
 bot = telebot.TeleBot(os.environ.get(config.env_key, None))
 
+message_types = namedtuple('MessageType', 'translate, punch, skip')(
+    'translate', 'punch', 'skip')
 
-def translate(text, date):
-    if date - config.old_date > config.time_offset:
-        try:
-            translate_message = translator(config.lang_from, config.lang_to, text)
-            return translate_message[0]
-        except:
-            print('Too many Requests')
-    else:
-        print('ждем еще', config.old_date, date)
+SKIP_MESS = "Парни сори, я пас, сегодня я каблук"
+
+
+def _is_translate_time(message):
+    if all([
+        config.period.translate_count > config.period.translate_period,
+        message.date - config.period.last_message_data > config.offset,
+        len(message.text) > config.message_length,
+    ]):
+        config.period.translate_count = 0
+        config.period.last_message_data = message.date
+        return True
+    return False
+
+
+def _is_punch_time(message):
+    if all([
+        config.period.punch_count > config.period.punch_period,
+        message.date - config.period.last_punch_data > config.offset,
+    ]):
+        config.period.punch_count = 0
+        config.period.last_punch_data = message.date
+        return True
+    return False
+
+
+def spot_answer_type(message):
+    if message.text.lower() in ["-", "пас", "я пас"]:
+        return message_types.skip
+
+    elif message.from_user.username in config.users:
+        return message_types.translate if _is_translate_time(message) else None
+
+    return message_types.punch if _is_punch_time(message) else None
+
+
+def translate(text):
+    try:
+        translate_message = translator(config.lang_from, config.lang_to, text)
+        return translate_message[0]
+    except Exception:
         return
-
-
-def pass_exceptions():
-    return "Парни сори, я пас, сегодня я каблук"
-
-
-def random_punch():
-    return random.choice(config.phrases)
-
-
-@bot.message_handler(commands=['punch'])
-def send_text(message):
-    bot.send_message(message.chat.id, random_punch())
 
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
-    user = message.from_user.username
-    text = message.text
-    chat_id = message.chat.id
+    config.period.punch_count += 1
+    config.period.translate_count += 1
 
-    if message.text.lower() in ["-", "пас", "я пас"]:
-        bot.send_message(chat_id, pass_exceptions())
-    elif user in config.users and len(text) > 10:
-        translate_text = translate(text, message.date)
-        if translate_text:
-            bot.send_message(chat_id, translate_text)
-            config.old_date = message.date
+    type_ = spot_answer_type(message)
+
+    mess = None
+    if type_ == message_types.translate:
+        mess = translate(message.text)
+
+    elif type_ == message_types.punch:
+        mess = random.choice(config.phrases)
+
+    elif type_ == message_types.skip:
+        mess = SKIP_MESS
+
+    return bot.send_message(message.chat.id, mess) if mess else None
 
 
-bot.polling()
+if __name__ == "__main__":
+    bot.polling()
--- config.py
+++ config.py
@@ -1,19 +1,32 @@
-from datetime import datetime
+from datetime import datetime as dt
+
+PHRASES = [
+    "пернул мозгом",
+    "да, ништяк",
+    "хуй в кармане ночевал",
+    "ключи от жопы потерял",
+    "ебануться туфли гнутся",
+    "кому тут отстрочить",
+    "Пожил блять",
+]
+
+
+class PeriodConfig:
+    translate_period = 3
+    translate_count = 0
+    last_message_data = dt.now().timestamp()
+
+    punch_period = 25
+    punch_count = 0
+    last_punch_data = dt.now().timestamp()
 
 
 class Config:
     env_key = 'TELE_KEY'
     lang_to = 'be'
     lang_from = 'ru'
-    time_offset = 300 # секунд до повторной отправки (5мин)
+    offset = 300
+    message_length = 15
     users = ['eromanovskyj', 'dm_melnikov']
-    old_date = datetime.today().timestamp() - 280
-    phrases = [
-        "пернул мозгом",
-        "да, ништяк",
-        "хуй в кармане ночевал",
-        "ключи от жопы потерял",
-        "ебануться туфли гнутся",
-        "кому тут отстрочить",
-        "Пожил блять"
-    ]
+    phrases = PHRASES
+    period = PeriodConfig
