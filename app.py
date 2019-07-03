b38e494c26c665f13c81baa48e3086db24263a8c
Viktor Shcheglov <v-shcheglov@yandex-team.ru>
2019-07-04 01:10:41 +0300

    add facts

--- app.py
+++ app.py
@@ -8,9 +8,10 @@ from collections import namedtuple
 
 config = Config()
 bot = telebot.TeleBot(os.environ.get(config.env_key, None))
+fact_ending = os.environ.get(config.env_fact_end, None)
 
-message_types = namedtuple('MessageType', 'translate, punch, skip')(
-    'translate', 'punch', 'skip')
+message_types = namedtuple('MessageType', 'translate, punch, skip, fact')(
+    'translate', 'punch', 'skip', 'fact')
 
 SKIP_MESS = "Парни сори, я пас, сегодня я каблук"
 
@@ -20,6 +21,7 @@ def _is_translate_time(message):
         config.period.translate_count > config.period.translate_period,
         message.date - config.period.last_message_data > config.offset,
         len(message.text) > config.message_length,
+        not message.text.startswith('http'),
     ]):
         config.period.translate_count = 0
         config.period.last_message_data = message.date
@@ -38,11 +40,20 @@ def _is_punch_time(message):
     return False
 
 
+def _is_fact_time(message):
+    return message.date - config.period.last_fact_data > config.long_offset
+
+
 def spot_answer_type(message):
-    if message.text.lower() in ["-", "пас", "я пас"]:
+    text = message.text.lower()
+    if text in ["-", "пас", "я пас"]:
         return message_types.skip
 
+    # logic only for special users
     elif message.from_user.username in config.users:
+        if config.trigger_word in text and _is_fact_time(message):
+            return message_types.fact
+
         return message_types.translate if _is_translate_time(message) else None
 
     return message_types.punch if _is_punch_time(message) else None
@@ -56,13 +67,24 @@ def translate(text):
         return
 
 
+def fact_type():
+    fact = random.choice(config.facts)
+
+    return '{} (c) {}'.format(fact, fact_ending)
+
+
 @bot.message_handler(commands=['punch'])
 def send_punch(message):
-    phrase = random.choice(config.phrases)
-    if 'https://' not in phrase:
-        phrase = phrase.capitalize()
+    phrase = random.choice(config.phrases).capitalize()
 
     bot.send_message(message.chat.id, phrase)
+
+
+@bot.message_handler(commands=['fact'])
+def send_fact(message):
+    fact = fact_type()
+
+    bot.send_message(message.chat.id, fact)
 
 
 @bot.message_handler(content_types=['text'])
@@ -81,6 +103,11 @@ def send_text(message):
 
     elif type_ == message_types.skip:
         mess = SKIP_MESS
+
+    elif type_ == message_types.fact:
+        mess = fact_type()
+        bot.reply_to(message, mess)
+        return
 
     if mess:
         bot.send_message(message.chat.id, mess)
