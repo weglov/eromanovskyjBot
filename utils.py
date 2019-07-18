d6e33a793e479016d3c3cff7f4db81db9d122514
Viktor Shcheglov <v-shcheglov@yandex-team.ru>
2019-07-18 15:06:13 +0300

    add remove handler

--- utils.py
+++ utils.py
@@ -9,6 +9,25 @@ config = Config()
 fact_ending = os.environ.get(config.env_fact_end, None)
 
 
+def with_remove(bot):
+    def decorator_handler(fn):
+        def wrapper(*args, **kw):
+            body = args[0]
+            text = body.text
+
+            if any([word in text for word in config.remove_commands]):
+                print(f"Triggering remove: {text} | {body.from_user.username}")
+
+                try:
+                    bot.delete_message(body.chat.id, body.message_id)
+                except Exception:
+                    pass
+
+            return fn(*args, **kw)
+        return wrapper
+    return decorator_handler
+
+
 def _is_translate_time(message):
     if all([
         config.period.translate_count > config.period.translate_period,
